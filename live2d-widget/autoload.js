d53b74e03a49bafea7fc0db1015b448bbfccb2dd
zhaopengzpz <3056150880@qq.com>
2019-07-15 21:19:14 +0800

    Site updated: 2019-07-15 21:19:13

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,4 +1,4 @@
- live2d_path = "/live2d-widget/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
 
 //加载waifu.css
 $("<link>").attr({ href: live2d_path + "waifu.css", rel: "stylesheet" }).appendTo("head");
